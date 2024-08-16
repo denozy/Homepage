b12cf2e456a9fecf185c6ae12cba1ad7d9552599
Nick <ndenbleyker@gmail.com>
2024-08-15 22:37:57 -0400

    update - adds layout and pages for react-router-dom

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import ProjectsContainer from "../components/ProjectsContainer";
+import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <ProjectsContainer />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
