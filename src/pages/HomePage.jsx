8614cc9aca634e162e59285e93759131a2020db2
Nick <ndenbleyker@gmail.com>
2024-08-21 11:20:02 -0400

    Build - adds Hero and Project container to serve as homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import Hero from "../components/Hero";
 import ProjectsContainer from "../components/ProjectsContainer";
-import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <ProjectsContainer />
-      <Footer />
     </>
   );
 };
