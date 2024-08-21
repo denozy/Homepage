0880f30060eea8ce512fd981fc53ed6460b5e5dc
Nick <ndenbleyker@gmail.com>
2024-08-21 13:41:50 -0400

    page - adds and styles not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,18 @@
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styles from "../styles/notFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section className={styles.section}>
+      <FaExclamationTriangle className={styles.icon} />
+      <h1 className={styles.title}>404 Not Found</h1>
+      <p className={styles.message}>This page does not exist</p>
+      <Link to="/" className={styles.link}>
+        Go Back
+      </Link>
+    </section>
+  );
 };
 
 export default NotFoundPage;
