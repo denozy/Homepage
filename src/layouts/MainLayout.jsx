73805a82e7e5ef9c475791b1de837d16468e5e02
Nick <ndenbleyker@gmail.com>
2024-08-21 11:22:33 -0400

    style - adds outletContainer for general styling, Nav and Footer added to layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
     <>
       <Nav />
-      <Outlet />
+      <div className="outletContainer">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 };
