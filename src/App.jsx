190e273bab53e6a262fea6ec4a3b94d5185dd95c
Nick <ndenbleyker@gmail.com>
2024-08-22 18:33:18 -0400

    updates routing to include singe post and project pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import PostsPage from "./pages/PostsPage";
+import SinglePostPage from "./pages/SinglePostPage";
 import ProjectsPage from "./pages/ProjectsPage";
+import SingleProjectPage from "./pages/SingleProjectPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
@@ -18,7 +20,9 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="Posts" element={<PostsPage />} />
+        <Route path="Posts/:slug" element={<SinglePostPage />} />
         <Route path="Projects" element={<ProjectsPage />} />
+        <Route path="Projects/:slug" element={<SingleProjectPage />} />
         <Route path="About" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
