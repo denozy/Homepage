a80d6e10ebd8def0be7d625b9d0537017f7d710f
Nick <ndenbleyker@gmail.com>
2024-08-28 13:57:51 -0400

    removed basename from router

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <Router basename="/homepage">
+    <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
