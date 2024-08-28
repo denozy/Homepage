1872e293bcec572d18827af1a599bf977404191e
Nick <ndenbleyker@gmail.com>
2024-08-28 12:41:01 -0400

    bugfix - added homepage to routing

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <Route path="Projects" element={<ProjectsPage />} />
         <Route path="Projects/:slug" element={<SingleProjectPage />} />
         <Route path="About" element={<AboutPage />} />
+        <Route path="homepage" element={<MainLayout />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
